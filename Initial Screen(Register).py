6a3e06033641c1bbd45a52eccf4103ebfff0c955
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-08-20 07:27:19 +0000

    Automated autoyapf fixes

--- Initial Screen(Register).py
+++ Initial Screen(Register).py
@@ -1,10 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
 
+
 def login():
+
     def clear_login():
         name_entry.delete(0, END)
         pass_entry.delete(0, END)
+
     Label(root, text="Name", font=10).place(x=600, y=380)
     Label(root, text="Password", font=10).place(x=600, y=410)
 
@@ -16,15 +19,25 @@ def login():
     pass_entry = Entry(root, font=10, textvariable=pass_info)
     pass_entry.place(x=700, y=410)
 
-    Button(root, text="Enter", font=("BankGothic Lt BT", 15, "bold"), ).place(x=600, y=470)
-    Button(root, text="Clear", font=("BankGothic Lt BT", 15, "bold"),command=clear_login).place(x=700, y=470)
+    Button(
+        root,
+        text="Enter",
+        font=("BankGothic Lt BT", 15, "bold"),
+    ).place(x=600, y=470)
+    Button(root,
+           text="Clear",
+           font=("BankGothic Lt BT", 15, "bold"),
+           command=clear_login).place(x=700, y=470)
+
 
 def register():
+
     def clear_register():
         name_entry.delete(0, END)
         rollno_entry.delete(0, END)
         email_entry.delete(0, END)
         pass_entry.delete(0, END)
+
     def pass_check(password):
         l = 0
         u = 0
@@ -42,10 +55,12 @@ def register():
                     p += 1
         else:
             return False
-        if (l >= 1 and u >= 1 and p >= 1 and d >= 1 and l + p + u + d == len(password)):
+        if (l >= 1 and u >= 1 and p >= 1 and d >= 1
+                and l + p + u + d == len(password)):
             return True
         else:
             return False
+
     def check_credentials():
         name = name_info.get()
         rollno = rollno_info.get()
@@ -64,16 +79,22 @@ def register():
             pass
 
         if pass_check(password):
-            Label(root, text="REGISTRATION SUCCESSFUL", font=("BankGothic Lt BT", 15), fg="green").place(x=30, y=580)
-            Button(root, text="Proceed ->", font=("BankGothic Lt BT", 18, "bold")).place(x=650, y=550)
+            Label(root,
+                  text="REGISTRATION SUCCESSFUL",
+                  font=("BankGothic Lt BT", 15),
+                  fg="green").place(x=30, y=580)
+            Button(root,
+                   text="Proceed ->",
+                   font=("BankGothic Lt BT", 18, "bold")).place(x=650, y=550)
         else:
-            messagebox.showerror("ERROR","Enter a valid password. Conditions:\n1.Lenght minimum 8 characters\n"
-                                         "2.Minimum one lowercase, uppercase, special charater.")
+            messagebox.showerror(
+                "ERROR",
+                "Enter a valid password. Conditions:\n1.Lenght minimum 8 characters\n"
+                "2.Minimum one lowercase, uppercase, special charater.")
         file = open("Records.txt", "a")
         file.write(name + " ")
         file.write(password + "\n")
         file.close()
-
 
     # Credentials for the form
     Label(root, text="Name", font=10).place(x=30, y=380)
@@ -97,8 +118,14 @@ def register():
     pass_entry.place(x=150, y=470)
 
     #Submit Button
-    Button(root, text="Submit", font=("BankGothic Lt BT", 15, "bold"),command=check_credentials).place(x=30, y=530)
-    Button(root, text="Clear", font=("BankGothic Lt BT", 15, "bold"),command=clear_register).place(x=150, y=530)
+    Button(root,
+           text="Submit",
+           font=("BankGothic Lt BT", 15, "bold"),
+           command=check_credentials).place(x=30, y=530)
+    Button(root,
+           text="Clear",
+           font=("BankGothic Lt BT", 15, "bold"),
+           command=clear_register).place(x=150, y=530)
     name = name_info.get()
     rollno = rollno_info.get()
     email = email_info.get()
@@ -116,18 +143,28 @@ root.geometry("1044x1044")
 root.title("SomaiyaPay")
 root.resizable(False, False)
 #verical and horizontal red lines
-text1 = Label(text = "Welcome to SomaiyaPay",bg = 'Red', fg = 'Black', padx = 233, pady = 4,
-              font = ("BankGothic Lt BT", 18, "bold"), borderwidth = 3).pack(fill=X)
-text2 = Label(text = "   ",bg = 'Red', borderwidth = 3).pack(side=LEFT, fill=Y)
+text1 = Label(text="Welcome to SomaiyaPay",
+              bg='Red',
+              fg='Black',
+              padx=233,
+              pady=4,
+              font=("BankGothic Lt BT", 18, "bold"),
+              borderwidth=3).pack(fill=X)
+text2 = Label(text="   ", bg='Red', borderwidth=3).pack(side=LEFT, fill=Y)
 
 #KJSCE logo
-photo = PhotoImage(file = "KJSCE LOGO.PNG")
-kjsce_logo = Label(image = photo)
+photo = PhotoImage(file="KJSCE LOGO.PNG")
+kjsce_logo = Label(image=photo)
 kjsce_logo.pack()
 
 #Buttons - Register and LogIn
-Button(root,text="Register", font=("BankGothic Lt BT", 18, "bold"),command=register).place(x=250, y=300)
-Button(root,text="Log In", font=("BankGothic Lt BT", 18, "bold"),command=login).place(x=650, y=300)
-
+Button(root,
+       text="Register",
+       font=("BankGothic Lt BT", 18, "bold"),
+       command=register).place(x=250, y=300)
+Button(root,
+       text="Log In",
+       font=("BankGothic Lt BT", 18, "bold"),
+       command=login).place(x=650, y=300)
 
 root.mainloop()
